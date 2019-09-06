e4168afd9cb19bb605be6012517147617409a675
Jenn Soderborg <jsoderborg7@gmail.com>
2019-09-05 19:19:55 -0600

    testing working currently, fingers crossed

--- baseball/src/App.test.js
+++ baseball/src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render } from '@testing-library/react';
+import App, {addBall} from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+test('App renders without crashing', () => {
+  render(<App />);
 });
+
+test("contains ball button", () => {
+  const myDoc = render(<App />)
+  myDoc.getByText(/ball/i);
+})
